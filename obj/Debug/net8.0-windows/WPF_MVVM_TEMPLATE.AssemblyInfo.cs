0834745e37c8c1a58a3a95386fae2061f06dc1dd
Mads Knudsen <mads@rknudsen.dk>
2024-09-26 15:39:53 +0200

    style(App.xaml): Added Styling to Labels.

--- obj/Debug/net8.0-windows/WPF_MVVM_TEMPLATE.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WPF_MVVM_TEMPLATE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_MVVM_TEMPLATE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9dcd018474b477a9c102daf9e667d52197db0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d913ea9c933bfedd528871de65efc5b1549570b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_MVVM_TEMPLATE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_MVVM_TEMPLATE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
