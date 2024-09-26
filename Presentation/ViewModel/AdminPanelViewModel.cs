c9dcd018474b477a9c102daf9e667d52197db0eb
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-09-26 15:07:11 +0200

    chore(UI): AdminPanel
    changed to more abst base in obs list.

--- Presentation/ViewModel/AdminPanelViewModel.cs
+++ Presentation/ViewModel/AdminPanelViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using WPF_MVVM_TEMPLATE.Application.Usecases;
 using WPF_MVVM_TEMPLATE.Entitys;
 using WPF_MVVM_TEMPLATE.Infrastructure;
@@ -11,56 +12,64 @@ namespace WPF_MVVM_TEMPLATE.Presentation.ViewModel;
 
 public class AdminPanelViewModel : ViewModelBase
 {
-    private ObservableCollection<ChatComp> _chatComps;
-
-    public ObservableCollection<ChatComp> ChatComps
+    private ObservableCollection<StackPanel> _UIComps = new ObservableCollection<StackPanel>();
+    public ObservableCollection<StackPanel> ChatComps
     {
-       get => _chatComps;
-       set => _chatComps = value;
+       get => _UIComps;
+       set => _UIComps = value;
     }
+    
     private List<Chat> _chats;
     private string _searchTerm;
+    public string SearchTerm
+    {
+       get => _searchTerm;
+       set
+       {
+          //Check if its one word without spaces.
+          if (WordService.CheckOneWord(value))
+          {
+             _searchTerm = value;
+             OnPropertyChanged(nameof(SearchTerm));
+             Debug.WriteLine($"SearchTerm: {_searchTerm}");
+          }
+          else
+          {
+             string message = $"Incorrect Search Term: {value}\n" +
+                              "Please enter one word without numbers or symbols."; 
+
+             // Display the message box
+             MessageBox.Show(message, "Search Failed", MessageBoxButton.OK, MessageBoxImage.Information);
+          }
+       }
+    }
+
 
     public AdminPanelViewModel()
     {
         _chats = LoadChats();
-        _chatComps = new ObservableCollection<ChatComp>();
-        foreach (var chat in _chats)
-        {
-           _chatComps.Add(new ChatComp(chat));
-        }
-        
-        
+        _UIComps = DisplayChats(_chats);
+
     }
     
-   private List<Chat> LoadChats()
+    private List<Chat> LoadChats()
     {
         IFileService fileService = new FileService(); 
         IChatRepos chatRepos = new XMLFileChatRepos(fileService);
         LoadChat loadChat = new LoadChat(chatRepos, "C:\\Users\\elias\\RiderProjects\\PELIOS\\Resources\\XML_DTD\\XMLFiles");
         return loadChat.GetAllChats(); 
     }
-   public string SearchTerm
-   {
-      get => _searchTerm;
-      set
-      {
-         //Check if its one word without spaces.
-         if (WordService.CheckOneWord(value))
-         {
-            _searchTerm = value;
-            OnPropertyChanged(nameof(SearchTerm));
-            Debug.WriteLine($"SearchTerm: {_searchTerm}");
-         }
-         else
-         {
-            string message = $"Incorrect Search Term: {value}\n" +
-                             "Please enter one word without numbers or symbols."; 
 
-            // Display the message box
-            MessageBox.Show(message, "Search Failed", MessageBoxButton.OK, MessageBoxImage.Information);
-         }
-      }
-   }
+    private ObservableCollection<StackPanel> DisplayChats(List<Chat> chats)
+    {
+       ObservableCollection<StackPanel> chatComps = new ObservableCollection<StackPanel>();
+       foreach (var chat in _chats)
+       {
+          chatComps.Add(new ChatComp(chat));
+       }
+
+       return chatComps; 
+
+    }
    
 }
